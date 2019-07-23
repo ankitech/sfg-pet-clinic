53e50377414fb11d75e057f6a42c5a28782631b7
kumar ankit <ankitech@gmail.com>
2019-07-23 23:53:05 +0530

    Create vet index page and Controllers. closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetsController {
+
+    @GetMapping(value = {"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";
+    }
+}
