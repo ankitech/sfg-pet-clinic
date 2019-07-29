cd506e0f1a668ed5c63bfed38ca8f3ea243ffc18
kumar ankit <ankitech@gmail.com>
2019-07-29 09:15:10 +0530

    Enhance owners with contact info and pets on startup. Closes #35

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
@@ -8,6 +9,8 @@ import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -38,12 +41,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Kumar");
         owner1.setLastName("Ankit");
+        owner1.setAddress("kunal icon");
+        owner1.setCity("pune");
+        owner1.setTelephone("123456789");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setName("Sandy");
+        mikesPet.setPetType(dog);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Smita");
         owner2.setLastName("Murmu");
+        owner2.setAddress("palm breeze");
+        owner2.setCity("banglore");
+        owner2.setTelephone("987466213");
+
+        Pet smitaPet = new Pet();
+        smitaPet.setName("Fluffy");
+        smitaPet.setPetType(cat);
+        smitaPet.setOwner(owner2);
+        smitaPet.setBirthDate(LocalDate.now());
+        owner2.getPets().add(smitaPet);
 
         ownerService.save(owner2);
 
